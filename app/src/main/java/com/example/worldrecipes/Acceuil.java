de4a6aeb4db8459da362ffe61e818ccc310ba38d
Lansana Diakite <diakslead@gmail.com>
2023-03-12 20:28:39 +0100

    not an important commit

--- app/src/main/java/com/example/worldrecipes/Acceuil.java
+++ app/src/main/java/com/example/worldrecipes/Acceuil.java
@@ -2,14 +2,24 @@ package com.example.worldrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
 
+
+
 public class Acceuil extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceuil);
 
+
+
     }
+
+
 }
